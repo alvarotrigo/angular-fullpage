18c10509564360d31ac0c62ba77137ce76300110
vh13294 <huotvongpanha@gmail.com>
2018-10-21 16:39:43 +1100

    test => add/remove sections

--- src/app/test-one/test-one.component.ts
+++ src/app/test-one/test-one.component.ts
@@ -64,16 +64,30 @@ export class TestOneComponent implements OnInit {
 
   testRebuild() {
     // change background color
-    this.config['sectionsColor'] = ['#a06080', '#a06080', '#a06080', '#a06080', '#a06080'];
+    this.config['sectionsColor'] = Array(6).fill(0).map(x => this.randomColor());
 
     // creating the section div
     const section = this.renderer.createElement('div');
     this.renderer.addClass(section, 'section');
     this.renderer.setProperty(section, 'innerHTML', '<h3>New Section</h3>');
-
     // adding section
     this.renderer.appendChild(this.fp_directive.nativeElement, section);
 
     this.fullpage_api.build();
+  }
+
+  removeLast() {
+    const lastSection = this.fp_directive.nativeElement.lastChild;
+
+    if (lastSection.isEqualNode(this.fullpage_api.getActiveSection().item)) {
+      this.fullpage_api.moveSectionUp();
+    }
+
+    lastSection.remove();
+    this.fullpage_api.build();
+  }
+
+  randomColor() {
+    return '#' + Math.random().toString(16).slice(-3);
   }
 }
