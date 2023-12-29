b9808adfe2499c12707ebd1fdc373fd67e80e85d
Ali Abdul Wahid <aly.dihaw96@gmail.com>
2023-12-29 13:22:47 +0700

    - Support Angular 12 to 16   - Downgrade to Angular 12 Version   - Add Demo/Example for Angular 12   - Add Demo/Example for Angular 13   - Add Demo/Example for Angular 14   - Add Demo/Example for Angular 15   - Add Demo/Example for Angular 16

---
+++ demo/angular13/app/examples/dynamic-changes/dynamic-changes.component.ts
@@ -0,0 +1,74 @@
+import { Component, OnInit, Renderer2, ViewChild, ElementRef } from '@angular/core';
+
+@Component({
+  selector: 'app-dynamic-changes',
+  templateUrl: './dynamic-changes.component.html',
+  styleUrls: ['./dynamic-changes.component.scss']
+})
+export class DynamicChangesExampleComponent implements OnInit {
+  @ViewChild('fullpageRef') fp_directive: ElementRef;
+  config;
+  fullpageApi;
+
+  constructor(private renderer: Renderer2) {
+
+    // this is just an example => for more details on config please visit fullPage.js docs
+    this.config = {
+      licenseKey: 'YOUR LICENSE KEY HERE',
+      anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'lastPage'],
+      menu: '#menu',
+      navigation: true,
+      sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', 'whitesmoke', '#ccddff'],
+
+      // events callback
+      afterLoad: (origin, destination, direction) => {
+        // console.log(destination);
+      },
+      afterRender: () => {
+        // console.log('afterRender');
+      },
+      afterResize: (width, height) => {
+        // console.log('afterResize' + width + ' ' + height);
+      },
+      afterSlideLoad: (section, origin, destination, direction) => {
+        // console.log(destination);
+      }
+    };
+  }
+
+  ngOnInit() {
+  }
+
+  getRef(fullPageRef) {
+    this.fullpageApi = fullPageRef;
+  }
+
+  addSection() {
+    // change background color
+    this.config['sectionsColor'] = Array(6).fill(0).map(x => this.randomColor());
+
+    // creating the section div
+    const section = this.renderer.createElement('div');
+    this.renderer.addClass(section, 'section');
+    this.renderer.setProperty(section, 'innerHTML', '<h3>New Section</h3>');
+    // adding section
+    this.renderer.appendChild(this.fp_directive.nativeElement, section);
+
+    this.fullpageApi.build();
+  }
+
+  removeLast() {
+    const lastSection = this.fp_directive.nativeElement.lastChild;
+
+    if (lastSection.isEqualNode(this.fullpageApi.getActiveSection().item)) {
+      this.fullpageApi.moveSectionUp();
+    }
+    lastSection.remove();
+
+    this.fullpageApi.build();
+  }
+
+  randomColor() {
+    return '#' + Math.random().toString(16).slice(-3);
+  }
+}
---
+++ demo/angular13/app/examples/dynamic-ng-for/dynamic-ng-for.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit, AfterViewInit, ViewChildren, QueryList } from '@angular/core';
+
+@Component({
+  selector: 'app-dynamic-ng-for',
+  templateUrl: './dynamic-ng-for.component.html',
+  styleUrls: ['./dynamic-ng-for.component.scss']
+})
+export class DynamicNgForComponent implements OnInit, AfterViewInit {
+  @ViewChildren('sectionRef') sectionRef: QueryList<any>;
+  config: any;
+  fullpageApi: any;
+  sections = [1];
+
+  constructor() {
+    // this is just an example => for more details on config please visit fullPage.js docs
+    this.config = {
+      licenseKey: 'YOUR LICENSE KEY HERE',
+      anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'lastPage'],
+      menu: '#menu',
+      navigation: true,
+      sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', 'whitesmoke', '#ccddff'],
+    };
+  }
+
+  ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    this.sectionRef.changes.subscribe(() => {
+      this.ngForRendred();
+    });
+  }
+
+  getRef(fullPageRef) {
+    this.fullpageApi = fullPageRef;
+  }
+
+  add() {
+    const twoDigits = Math.random() * 100;
+    this.sections.push( Math.round(twoDigits) );
+  }
+
+  ngForRendred() {
+    console.log('ngFor dom render is done');
+    this.fullpageApi.build();
+  }
+
+}
